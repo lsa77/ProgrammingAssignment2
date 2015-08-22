f6fce2277f0a1cb1a7f9580b4fa6c76be3534799
lsa77 <lukasz.sawicki.2@gmail.com>
2015-08-22 15:35:38 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+##I couldn't figure out why do I need a matrix here so I decided to stay with a vector.
+##this works as in dr. Peng's example but in matrix context
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverted <- NULL
+    set <- function(y) {
+        x <<- y
+        inverted <<- NULL
+    }
+    get <- function() x
+    set_inverted <- function(solve) inverted <<- solve
+    get_inverted <- function() inverted
+    list(set = set, get = get,
+         set_inverted = set_inverted,
+         get_inverted = get_inverted)
 }
 
-
-## Write a short comment describing this function
-
+##cacheSolve checks if there is an inverted version of the matrix in the cache. if there is one - returns it
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inverted <- x$get_inverted()
+    
+    if(!is.null(inverted)) {
+        message("getting cached data")
+        return(inverted)
+    }
+    
+    data <- x$get()
+    inverted <- solve(data, ...)
+    x$set_inverted(inverted)
+    inverted
 }
+    
+
